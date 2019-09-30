9647a7735248a5689f2d76a214da635e739aeadf
sbravor64 <Andresbravo97>
2019-09-30 16:49:10 +0200

    Initial commit

---
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.widgets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+
+    Switch aSwitch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                
+            }
+        });
+
+
+    }
+}
